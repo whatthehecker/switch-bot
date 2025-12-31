1cb257d1f9b2d6aed175a148bf67685c32f887c7
Robert Hecker <roberthecker@gmx.net>
2025-12-31 15:09:07 +0100

    Fix some smaller warnings/errors caused by using an older Python version

--- backend/programs/bdsp_soft_resetter/battle_entry_presets.py
+++ backend/programs/bdsp_soft_resetter/battle_entry_presets.py
@@ -1,6 +1,8 @@
+from typing import Dict
+
 from programs.bdsp_soft_resetter.battle_entry import BattleEntryDelegate, BattleEntry
 
-PRESET_BATTLE_ENTRIES: {str: BattleEntryDelegate} = {
+PRESET_BATTLE_ENTRIES: Dict[str, BattleEntryDelegate] = {
     'Registeel': BattleEntry.simple(pre_battle_animation_timeout=4.0, animation_timeout=7.0),
     'Regirock': BattleEntry.simple(pre_battle_animation_timeout=4.0, animation_timeout=7.0),
     'Regice': BattleEntry.simple(pre_battle_animation_timeout=4.0, animation_timeout=7.0),
